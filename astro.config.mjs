045f96f6c1c9293a240a91ed93f6f24679cd94b2
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2024-08-03 08:14:20 +0800

    feat(astro.config): change Astro.url

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://blog.hansputera.dev/",
   base: "/",
   trailingSlash: "always",
   integrations: [
