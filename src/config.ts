6ebd4fcc0c42f2190ec8dddbcb35a3eb0a6acf3b
hansputera <hanifdwyputrasembiring@gmail.com>
2024-08-25 03:25:51 +0800

    feat(*): add favicon
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -30,6 +30,10 @@ export const siteConfig: SiteConfig = {
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
+    {
+      src: 'https://i.imgur.com/cPWVVt9.jpeg',
+      theme: 'dark',
+    },
   ]
 }
 
